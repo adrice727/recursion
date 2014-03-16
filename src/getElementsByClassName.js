a23287ac6633185ab7e929cacc8af7261c2b4a84
Aaron Rice <adrice727@gmail.com>
2014-03-16 18:20:36 -0400

    Started work on stringifyJSON and getElementsByClass

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,5 +5,27 @@
 
 // But in stead we're going to implement it from scratch:
 var getElementsByClassName = function (className) {
-  // your code here
+  
+  // var result = {};
+
+
+  // function checkForClass(node){
+  //   console.log("node: " + node); 
+  //   var node = node || document.body;
+  //   var children = node.childNodes;
+  //   console.log(children);
+  //   if (node.classList !== undefined && node.classList.contains(className)) {
+  //     Array.prototype.push.call(result, node)
+  //   }
+  //   if (children){
+  //     for ( var key in children ) {
+  //       checkForClass(children[key], result)  
+  //     }
+  //   }
+  // }
+
+  // checkForClass();
+  
+  // return result;
+
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,62 @@
 
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
-  // your code goes here
-};
+
+  var result = "";
+
+
+  
+  if ( Object.prototype.toString.call(obj) === "[object Object]"){  
+    var obSize = Object.keys(obj).length;
+    if ( obSize === 0 ) {
+      result += "{}";
+    } else {
+      var i = 1;  //keeps track of which key-value pair is being stringified
+      result += "{";
+      for ( var key in obj ){
+        var theKey = key;
+        var theValue = obj[key];
+
+        if ( theKey !== undefined && typeof theKey !== 'function' && theValue !== undefined && typeof theValue !== 'function' ){
+          result += stringifyJSON(theKey) + ":" + stringifyJSON(theValue);
+          console.log("i: " + i + "; obSize: " + obSize );
+          if ( i < obSize ) result += ",";
+          i++;
+        }
+      }
+      result += "}";
+    }
+  } else if ( Array.isArray(obj) ) {
+    result += "[";
+    for ( var i = 0; i < obj.length; i++ ) {
+      var item = obj[i];
+      if (Array.isArray(item)) {
+        result += stringifyJSON(item);
+      } else {
+        if (typeof item === 'string'){
+          item = '"' + item + '"';
+        }
+        if ( i < obj.length - 1){
+          result += item + ",";
+        } else {
+        result += item;
+        }
+      }
+    } 
+    result += "]";
+  } else if ( typeof obj === "string" ) {
+    result += '"' + obj + '"';
+  } else if ( typeof obj === "number" ) {
+    result += obj;
+  } else if ( typeof obj === "boolean") {
+    result += obj;
+  } else if ( typeof obj === "function") {
+    console.log("Can't stringify a function");
+  } else if ( obj === null ) {
+    result += "null";
+  } else if ( obj === undefined ) {
+    console.log("Can't stringify undefined value");
+  }
+
+  return result;
+}
